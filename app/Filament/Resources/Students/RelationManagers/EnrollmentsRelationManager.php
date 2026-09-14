1e4762646cc33f0225d5078bc4bff6028320cc24
moneycold <xalimovahrorbek@gmail.com>
2026-09-14 15:16:59 +0500

    feat: remove Enroll in a course button from student view page

--- app/Filament/Resources/Students/RelationManagers/EnrollmentsRelationManager.php
+++ app/Filament/Resources/Students/RelationManagers/EnrollmentsRelationManager.php
@@ -3,8 +3,6 @@
 namespace App\Filament\Resources\Students\RelationManagers;
 
 use App\Filament\Resources\Enrollments\EnrollmentResource;
-use App\Filament\Resources\Students\StudentResource;
-use Filament\Actions\Action;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Support\Colors\Color;
 use Filament\Tables\Columns\TextColumn;
@@ -85,11 +83,6 @@ class EnrollmentsRelationManager extends RelationManager
             //         ->collapsible()
             // )
             ->recordUrl(fn ($record) => EnrollmentResource::getUrl('view', ['record' => $record]))
-            ->headerActions([
-                Action::make('enroll')
-                    ->label(__('Enroll in a course'))
-                    ->icon('heroicon-o-plus-circle')
-                    ->url(fn () => StudentResource::getUrl('enroll', ['record' => $this->getOwnerRecord()])),
-            ]);
+            ->headerActions([]);
     }
 }
